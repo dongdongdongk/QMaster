61f283337d04aeacbdf000538fc15df6de321770
dongdongdongk <dhk9309@gmail.com>
2025-04-17 17:46:29 +0900

    타이머 전환 추가가

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,19 +6,53 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Question")]
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSo questions;
+
+    [Header("Answer")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly = false;
+
+    [Header("ButtonColor")]
     [SerializeField] Sprite defaltAnswerButtonSprite;
     [SerializeField] Sprite correctAnswerButtonSprite;
+
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
     void Start()
     {
-        DisplayQuestion();
-        //GetNextQuestion();
+        timer = FindObjectOfType<Timer>();
+        // DisplayQuestion();
+        GetNextQuestion();
     }
 
+    void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!timer.isAnsweringQuestion && !hasAnsweredEarly)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
+    }
     public void OnAnswerSelected(int index)
+    {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        timer.CancelTimer();
+    }
+
+    void DisplayAnswer(int index)
     {
         Image buttonImage;
         if (index == questions.GetCorrectAnswerIndex())
@@ -34,9 +68,8 @@ public class Quiz : MonoBehaviour
             questionText.text = "오답입니다! 정답은 " + correctAnser + "입니다.";
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerButtonSprite;
-            
+
         }
-        SetButtonState(false);
     }
 
     void GetNextQuestion()
@@ -57,15 +90,19 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    void SetButtonState(bool state) {
-        for(int i = 0; i < answerButtons.Length; i++) {
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
             Button button = answerButtons[i].GetComponent<Button>();
             button.interactable = state;
         }
     }
 
-    void SetDefaultButtonSprites() {
-        for(int i = 0; i < answerButtons.Length; i++) {
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
             Image buttonImage = answerButtons[i].GetComponent<Image>();
             buttonImage.sprite = defaltAnswerButtonSprite;
         }
