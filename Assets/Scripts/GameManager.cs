13f120d8b8b3b43b39cf08c1bc8c8aaa0802fc4c
dhk939393 <dhk9309@gmail.com>
2025-04-22 23:51:29 +0900

    최종 게임 종료

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    
+    Quiz quiz;
+    EndScreen endScreen;
+    void Start()
+    {
+        quiz = FindObjectOfType<Quiz>();
+        endScreen = FindObjectOfType<EndScreen>();
+
+        quiz.gameObject.SetActive(true);
+        endScreen.gameObject.SetActive(false);
+        endScreen.ShowFinalScore();
+    }
+
+    void Update()
+    {
+        if (quiz.isComplete)
+        {
+            quiz.gameObject.SetActive(false);
+            endScreen.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnReplayLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
